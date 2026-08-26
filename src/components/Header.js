247dc867111a19ae5a69dc29a13376464a6c0862
Hridesh Bharati <hridesh027@gmail.com>
2026-08-26 21:25:20 +0530

    Update workflow and app.json configuration for ZapStore

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,6 @@ export default function Header({
               </Badge>
             )}
           </View>
-
           {/* Cart Icon */}
           <View style={styles.iconWrapper}>
             <IconButton
